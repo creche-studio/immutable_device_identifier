788cc3210bc07318b48431e1333cf2c16f78c891
Kohei Tabata <nerd0geek1@gmail.com>
2022-07-17 22:26:02 +0900

    implement MyApp in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,8 +31,7 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _immutableDeviceIdentifierPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _immutableDeviceIdentifierPlugin.getUniqueId();
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
