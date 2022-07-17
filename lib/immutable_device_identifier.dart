c225db42fb5a5183cc68f351abb0f9447c950dad
Kohei Tabata <nerd0geek1@gmail.com>
2022-07-17 22:18:08 +0900

    fix dart code, delete unused classes

--- lib/immutable_device_identifier.dart
+++ lib/immutable_device_identifier.dart
@@ -1,8 +1,18 @@
-
-import 'immutable_device_identifier_platform_interface.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
 class ImmutableDeviceIdentifier {
-  Future<String?> getPlatformVersion() {
-    return ImmutableDeviceIdentifierPlatform.instance.getPlatformVersion();
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('immutable_device_identifier');
+
+  Future<String> getUniqueId() async {
+    final uniqueId = await methodChannel.invokeMethod<String>('getUniqueId');
+
+    if (uniqueId == null) {
+      throw AssertionError('uniqueId become null unexpectedly.');
+    }
+
+    return uniqueId;
   }
 }
